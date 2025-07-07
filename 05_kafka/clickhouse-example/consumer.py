f1776ed3e0607495a5645110a8783ec9c1ac9e10
Marianna Alpatova <vrisk.serket@gmail.com>
2025-07-07 11:46:18 +0300

    Added debezium samples

--- 05_kafka/clickhouse-example/consumer.py
+++ 05_kafka/clickhouse-example/consumer.py
@@ -18,7 +18,7 @@ import clickhouse_connect
 
 consumer = KafkaConsumer(
     "user_events",
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers="localhost:29092",
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
